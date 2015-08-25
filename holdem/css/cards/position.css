f1946285959d1bcb959d19eb382affb911f15729
Zvarych Anton <zvarych.anton@gmail.com>
2015-08-25 20:04:35 +0300

    holdem

---
+++ holdem/css/cards/position.css
@@ -0,0 +1,50 @@
+#placeForBoard {
+	position:absolute;
+	margin-top: 300px;
+	margin-left:400px;
+}
+#player_1 {
+	position:absolute;
+	margin-left:200px;
+}
+#player_2 {
+	position:absolute;
+	margin-left:400px;
+}
+#player_3 {
+	position:absolute;
+	margin-left:600px;
+}
+#player_4 {
+	position:absolute;
+	margin-left:800px;
+}
+#player_5 {
+	position:absolute;
+	margin-left:1000px;
+}
+#player_6 {
+	position:absolute;
+	margin-top: 450px;
+	margin-left:200px;
+}
+#player_7 {
+	position:absolute;
+	margin-top: 450px;
+	margin-left:400px;
+}
+#player_8 {
+	position:absolute;
+	margin-top: 450px;
+	margin-left:600px;
+}
+#player_9 {
+	position:absolute;
+	margin-top: 450px;
+	margin-left:800px;
+}
+#player_10 {
+	position:absolute;
+	margin-top: 450px;
+	margin-left:1000px;
+}
